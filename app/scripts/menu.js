acc60adae9d015a63d228dbdb23c6510e0b73e75
Ricardo.Bravo <javier.tec1989@gmail.com>
2017-10-20 16:01:37 -0500

    enviando a correcion

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,7 +1,7 @@
 var menu = document.querySelector("#menu");
 
 var menuItems = [
-  {"item": "Tipos de Gastos", "active": false},
+  {"item": "Tipos de Gastos","state":"tipo", "active": false},
   {"item": "Detalle de Gastos", "state": "detalle", "active": true},
   {"item": "Gastos totales", "state": "totales", "active": false}
 ];
@@ -11,13 +11,17 @@ menu.innerHTML = PriceApp.menu(menuContext);
 
 var appContent = document.querySelector("#appContent");
 var blockInfo = {
-  "detale": [
+  "detalle": [
     {"blockName": "Gastos Mensual", "blockHeight": "150px"},
     {"blockName": "Detalle de Gastos", "blockHeight": "350px"},
   ],
   "totales": [
     {"blockName": "Filtros", "blockHeight": "200px"},
     {"blockName": "Gastos Totales por año", "blockHeight": "300px"},
+  ],
+  "tipo": [
+    {"blockName": "Tipo de Gastos", "blockHeight": "200px"},
+    {"blockName": "Categorias", "blockHeight": "300px"},
   ]
 }
 
@@ -30,6 +34,23 @@ function getStateTitle(state) {
   }
 }
 
+function combo() {
+  $('select').material_select();
+
+  $("button").click(function () {
+
+    $("select").empty().html(' ');
+
+    var value = "New value";
+    $("select").append(
+      $("<option></option>").attr("value", value).text(value)
+    );
+
+    $("select").material_select('update');
+    $("select").closest('.input-field').children('span.caret').remove();
+  });
+};
+
 function changeState(state) {
   var appContenContext = {"state": state, "title": getStateTitle(state)};
   appContent.innerHTML = PriceApp.content(appContenContext);
@@ -38,8 +59,40 @@ function changeState(state) {
 
   $(".menuLinks").removeClass("menuActive");
   $("#" + state + "Link").addClass("menuActive");
+  if (state === "detalle") {
+    var detalleInf = document.querySelector("#detalleInf");
+    var detInf = [
+      {"name": "Total Mes", "valor": 500},
+      {"name": "Gasto Maximo", "valor": 200},
+      {"name": "Gasto Minimo", "valor": 100},
+    ];
+    detalleInf.innerHTML = PriceApp.detalleInf({gastos: detInf});
+    var tableGastoInf = document.querySelector("#gastosTable");
+    var gastosInf = [
+      {
+        "categoria": "Gastos Fijos",
+        "subcategoria": "Telefono",
+        "fecha": "2017-09-14",
+        "detalle": "Gasto 1",
+        "valor": "100"
+      },
+      {"categoria": "Formacion", "subcategoria": "Cursos", "fecha": "2017-09-14", "detalle": "Gasto 2", "valor": "200"},
+      {
+        "categoria": "Transforme",
+        "subcategoria": "Combustible",
+        "fecha": "2017-09-14",
+        "detalle": "Gasto 3",
+        "valor": "200"
+      },
+    ];
+    tableGastoInf.innerHTML = PriceApp.tableGastos({gastost: gastosInf});
+    combo();
+  }else  if (state ==="tipo"){
+    combo();
+  }
 }
 
 changeState("detalle");
 $(".button-collapse").sideNav();
+
 
